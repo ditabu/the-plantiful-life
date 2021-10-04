ce27cb239523500f080e5afbbce8303a31b3eb31
DiAnne Lozano <diannelozano@DiAnnes-MacBook-Pro-2.local>
2021-10-04 14:37:31 -0700

    add plant request

--- controllers/plants.js
+++ controllers/plants.js
@@ -2,10 +2,16 @@ const Plant = require('../models/plant');
 
 
 module.exports = {
-    new: newPlant
+    new: newPlant,
+    create
 }
 
 function newPlant(req, res){
     console.log('add new plant')
     res.render('plants/new');
 };
+
+function create(req, res){
+    console.log(req.body)
+    res.send('added new plant')
+}
--- routes/plants.js
+++ routes/plants.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const plantsCtrl = require('../controllers/plants');
 
 router.get('/new', plantsCtrl.new);
+router.post('/plants', plantsCtrl.create);
 
 module.exports = router;
