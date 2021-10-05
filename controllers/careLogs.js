5728f6ba20e1d9dce0965973f26191684d56cf76
DiAnne Lozano <diannelozano@DiAnnes-MacBook-Pro-2.local>
2021-10-05 10:24:18 -0700

    care log page route

--- controllers/careLogs.js
+++ controllers/careLogs.js
@@ -6,6 +6,7 @@ module.exports = {
 
 function create(req, res){
     console.log(req.body, 'does this show up')
+    res.send('carlog page created')
     // CareLog.create(req.body, function(err, createdCareLog){
     //     if(err) return res.direct('/plants/:id');
     //     console.log(createdCareLog, 'createdCarelog error');
